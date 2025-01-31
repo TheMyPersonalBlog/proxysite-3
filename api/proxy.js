4fe6a86662d5d9755c1e187dcdb0d7ab86783d42
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-31 10:09:52 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://velocity.radon.games/";//your website url
+  let target = "https://games.crazygames.com/en_US/sky-riders-buk/index.html";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
